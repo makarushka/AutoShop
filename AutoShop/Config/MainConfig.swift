a130b2e053f1cbfa2a7cfecbfdeb87264a838f00
makarushka <yuri.makarow@ya.ru>
2019-01-01 21:46:30 +0300

    Refractoring

--- AutoShop/Config/MainConfig.swift
+++ AutoShop/Config/MainConfig.swift
@@ -19,10 +19,12 @@ class MainConfig {
         let vesta1 = FactoryCars.shared.creatorCars(typeCars: .vesta, hourseCount: 235, color: .white, maxSpeed: 230, priceCount: 400_000)
         let mersedes1 = FactoryCars.shared.creatorCars(typeCars: .mersedes, hourseCount: 350, color: .black, maxSpeed: 270, priceCount: 1_300_000)
         let audi1 = FactoryCars.shared.creatorCars(typeCars: .audi, hourseCount: 280, color: .orange, maxSpeed: 250, priceCount: 1_200_000)
+        let toyota1 = FactoryCars.shared.creatorCars(typeCars: .toyota, hourseCount: 230, color: .orange, maxSpeed: 250, priceCount: 1_200_300)
         DataSource.shared.allCars.append(kalina1)
         DataSource.shared.allCars.append(vesta1)
         DataSource.shared.allCars.append(mersedes1)
         DataSource.shared.allCars.append(audi1)
+        DataSource.shared.allCars.append(toyota1)
         
         DataSource.shared.allCars.forEach() {print("Машина \($0.description), с цветом \($0.color.rawValue) относится к \($0.countryDev.rawValue) и имеет VIN номер - \($0.vinNumber)")
             print("\($0.color)")
@@ -31,10 +33,12 @@ class MainConfig {
     }
     
     func createCategory() {
-        let categoryAuto = RussianCars.shared
-        let categoryRepair = GermanCars.shared
-        DataSource.shared.allCaregory.append(categoryAuto)
-        DataSource.shared.allCaregory.append(categoryRepair)
+        let russianCars = RussianCars.shared
+        let germanCars = GermanCars.shared
+        let japanCars = JapanCars.shared
+        DataSource.shared.allCaregory.append(russianCars)
+        DataSource.shared.allCaregory.append(germanCars)
+        DataSource.shared.allCaregory.append(japanCars)
     }
     
     private init() {}
