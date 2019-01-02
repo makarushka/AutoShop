fc12f2c908c255c057d6d1f31cbf5ea1988bcb4b
makarushka <yuri.makarow@ya.ru>
2019-01-03 02:54:05 +0300

    Refactoring

---
+++ AutoShop/Controllers/MainVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainVC.swift
+//  AutoShop
+//
+//  Created by Yuriy on 03/01/2019.
+//  Copyright © 2019 Yuriy. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
